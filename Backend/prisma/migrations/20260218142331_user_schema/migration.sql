3cadbd7c0d23c85a15492e74f321b5d3323a4572
Pradeep <pradeep2ndsingh@gmail.com>
2026-02-18 19:53:55 +0530

    updated user model

--- Backend/prisma/migrations/20260218142331_user_schema/migration.sql
+++ Backend/prisma/migrations/20260218142331_user_schema/migration.sql
@@ -1,12 +1,13 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
     "name" TEXT,
     "avatar" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
