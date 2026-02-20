dfa1e8fc0e1331e8cebbd2f473b58ac28d291e31
Pradeep <pradeep2ndsingh@gmail.com>
2026-02-20 22:54:14 +0530

    migration cleaning

--- Backend/prisma/migrations/20260220172320_init/migration.sql
+++ Backend/prisma/migrations/20260220172320_init/migration.sql
@@ -1,21 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `refreshToken` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "WorkspaceRole" AS ENUM ('OWNER', 'ADMIN', 'MEMBER');
-
--- DropForeignKey
-ALTER TABLE "refreshToken" DROP CONSTRAINT "refreshToken_userId_fkey";
-
--- DropTable
-DROP TABLE "refreshToken";
-
--- DropTable
-DROP TABLE "user";
 
 -- CreateTable
 CREATE TABLE "User" (
