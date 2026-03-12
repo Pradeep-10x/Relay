c6c259b4cb54cf5ce849d48fa3ca95be2a51a5e4
Pradeep <pradeep2ndsingh@gmail.com>
2026-03-13 01:41:15 +0530

    comment service for creating

---
+++ Backend/src/modules/comment/comment.service.ts
@@ -0,0 +1,53 @@
+import { prisma } from "../../lib/prisma.js";
+import { ApiError } from "../../utils/ApiError.js";
+
+
+export const createCommentService = async (
+  issueId: string,
+  userId: string,
+  content: string
+) => {
+     const issue = await prisma.issue.findUnique({
+      where: { id: issueId },
+      select: { id: true, projectId: true },
+     });
+
+     if (!issue) {
+      throw new ApiError(404, "Issue not found");
+     }
+
+     const membership = await prisma.projectMember.findUnique({
+      where: {
+        userId_projectId: {
+          userId,
+          projectId: issue.projectId,
+        },
+      },
+     });
+
+     if (!membership) {
+      throw new ApiError(403, "Not a project member");
+     }
+
+
+     const comment = await prisma.issueComment.create({
+      data: {
+        issueId,
+        userId,
+        content,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatar: true,
+          },
+        },
+      },
+     });
+
+     return comment;
+    };
+    
