9f1a0f804eb97464a1a966ac88b53c923207aeb0
Pradeep <pradeep2ndsingh@gmail.com>
2026-03-13 01:51:48 +0530

    controllers for comment service

---
+++ Backend/src/modules/comment/comment.controller.ts
@@ -0,0 +1,31 @@
+import { asyncHandler } from "../../utils/AsyncHandler.js";
+import { Request, Response } from "express";
+import { createCommentService, getIssueCommentsService, editCommentService, deleteCommentService } from "./comment.service.js";
+
+export const createComment = asyncHandler(async (req: Request, res: Response) => {
+    const { issueId, content } = req.body;
+    const userId = (req as any).user?.id;
+    const comment = await createCommentService(issueId, userId!, content);
+    res.status(201).json({ success: true, data: comment });
+});
+
+export const getIssueComments = asyncHandler(async (req: Request, res: Response) => {
+    const { issueId } = req.params;
+    const comments = await getIssueCommentsService(issueId as string);
+    res.status(200).json({ success: true, data: comments });
+});
+
+export const editComment = asyncHandler(async (req: Request, res: Response) => {
+    const { commentId } = req.params;
+    const { content } = req.body;
+    const userId = (req as  any).user?.id;
+    const comment = await editCommentService(commentId as string, userId!, content);
+    res.status(200).json({ success: true, data: comment });
+});
+
+export const deleteComment = asyncHandler(async (req: Request, res: Response) => {
+    const { commentId } = req.params;
+    const userId = (req as any).user?.id;
+    const comment = await deleteCommentService(commentId as string, userId!);
+    res.status(200).json({ success: true, data: comment });
+});
