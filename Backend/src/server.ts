8445a559113f929f32e028bdd89a64afabc6149d
Pradeep <pradeep2ndsingh@gmail.com>
2026-03-13 15:05:48 +0530

    server modify for socket

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,19 +2,23 @@ import app from "./app.js";
 import { env } from "./config/env.js";
 import { prisma } from "./lib/prisma.js";
 import { logger } from "./config/logger.js";
+import http from "http";
+import { initSocket } from "./lib/socket.js";
 
 async function startServer() {
   try {
     await prisma.$connect();
     logger.info("Database connected");
 
-    app.listen(env.PORT, () => {
+    const server = http.createServer(app);
+    initSocket(server);
+    server.listen(env.PORT, () => {
       logger.info(`Server running on port ${env.PORT}`);
     });
+
   } catch (error) {
     logger.error(error);
     process.exit(1);
   }
 }
-
 startServer();
