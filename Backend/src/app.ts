9e4400158ff376b8eb22e1e7148f71c3cb1719eb
Pradeep <pradeep2ndsingh@gmail.com>
2026-03-16 00:09:38 +0530

    cors fix for universal url

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -21,7 +21,10 @@ import { redis } from "./lib/redis.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
