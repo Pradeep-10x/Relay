4f3093af5efd40e5352e9a8e9aee8f54a684d442
Pradeep <pradeep2ndsingh@gmail.com>
2026-04-03 20:58:47 +0530

    ci cd check:

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-//app.set("trust proxy", true); 
+app.set("trust proxy", true); 
 app.use(rateLimiter);
 app.use(httpLogger);
 
